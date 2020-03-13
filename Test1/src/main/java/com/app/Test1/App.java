efc20be8377a40750a67baf7d009d7c0050e91f0
chaan <chaan@DESKTOP-68U3HT9>
2020-03-13 04:52:40 -0700

    git test hello

---
+++ Test1/src/main/java/com/app/Test1/App.java
@@ -0,0 +1,14 @@
+package com.app.Test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World8888!" );
+    }
+}
